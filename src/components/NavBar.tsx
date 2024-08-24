35444f8fd9e3ff93958c4a6bfea1f040dd5c0ab5
Edwin ayerbe <edwinayerbe38@gmail.com>
2024-08-24 18:44:36 +0100

    Home-page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,18 +1,41 @@
+import { useState } from "react";
 import logo from "../assets/home/logo.png";
 import "../styles/componets/navbar.css";
 
 type Props = {};
 
 function NavBar({}: Props) {
+  const [activeButton, setActiveButton] = useState<number | null>(null);
+
   return (
     <div className="container-header">
       <img className="logo" src={logo} alt="logo" />
       <nav className="navbar">
         <ul className="list">
-          <li>00 HOME</li>
-          <li>01 DESTINATION</li>
-          <li>02 CREW</li>
-          <li>03 TECHNOLOGY</li>
+          <button className="button-navbar" onClick={() => setActiveButton(0)}>
+            00 HOME
+            <span
+              className={activeButton === 0 ? "underline active" : "underline"}
+            ></span>
+          </button>
+          <button className="button-navbar" onClick={() => setActiveButton(1)}>
+            01 DESTINATION
+            <span
+              className={activeButton === 1 ? "underline active" : "underline"}
+            ></span>
+          </button>
+          <button className="button-navbar" onClick={() => setActiveButton(2)}>
+            02 CREW
+            <span
+              className={activeButton === 2 ? "underline active" : "underline"}
+            ></span>
+          </button>
+          <button className="button-navbar" onClick={() => setActiveButton(3)}>
+            03 TECHNOLOGY
+            <span
+              className={activeButton === 3 ? "underline active" : "underline"}
+            ></span>
+          </button>
         </ul>
       </nav>
     </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,6 +7,21 @@ function Home({}: Props) {
   return (
     <div className="container-home">
       <NavBar />
+      <div className="main">
+        <section className="section">
+          <h3 className="subtitle">SO, YOU WANT TO TRAVEL TO</h3>
+          <h1 className="title">SPACE</h1>
+          <p className="paragraph">
+            Let’s face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we’ll give you a truly out of this
+            world experience!
+          </p>
+        </section>
+        <div className="button-container">
+          <button className="button">Explore</button>
+        </div>
+      </div>
     </div>
   );
 }
